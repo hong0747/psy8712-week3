78c9cfdd75cb73ce09e3745e774b6c7230fda043
Julia Hong <hong0747@umn.edu>
2026-02-09 10:10:47 -0600

    Created clean_df to exclude research assistants and participants with failed attention checks

--- R/week3.R
+++ R/week3.R
@@ -3,3 +3,7 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 # Data Import and Cleaning
 raw_df <- read.csv(file = "../data/week3.csv")
+raw_df$timeStart <- as.POSIXct(raw_df$timeStart)
+raw_df$timeEnd <- as.POSIXct(raw_df$timeEnd)
+clean_df <- raw_df[!grepl("2017-06", raw_df$timeStart), ]
+clean_df <- clean_df[clean_df$q6 == "1", ]
